e99eca836732c24df0f40f3d6b0108eb9ba1b53f
Chien Wei, Lin <superfishman9@gmail.com>
2020-04-22 13:17:51 +0800

    Update promise.py

--- promise.py
+++ promise.py
@@ -1,20 +1,26 @@
 import asyncio
 
-Promise = Promised()
-
 class Promised:
-  async def all(self, tasks: list) -> list:
-    return await asyncio.gather(*taks)
+    def all(self, tasks: list) -> list:
+        return asyncio.run(self.asyncAll(*tasks))
+    
+    def race(self, tasks: list):
+        return asyncio.run(self.asyncRace(*tasks))
+    
+    async def asyncAll(self, tasks: list) -> list:
+        return await asyncio.gather(*tasks)
+    
+    async def asyncRace(self, tasks: list):
+        done, pendding = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        for p in pendding:
+            p.cancel()
+        return done.pop().result()
 
-  async def race(self, tasks: list):
-    done, pendding = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-    for p in pendding:
-        p.cancel()
-    return done.pop().result()
+Promise = Promised()
 
 def main():
   task = [test(i) for i in range(10)]
-  a = asyncio.run(Promise.race(task))
+  a = Promise.race(task)
   print(a)
 
 
